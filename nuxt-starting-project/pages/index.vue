95b81097713f6427852446a53243434fda3282b4
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2022-07-01 22:24:13 +0100

    Starting nuxt stuff

--- nuxt-starting-project/pages/index.vue
+++ nuxt-starting-project/pages/index.vue
@@ -3,11 +3,8 @@
     <div>
       <logo/>
       <h1 class="title">
-        nuxt-course
+        Playing around with Nuxt.js... again
       </h1>
-      <h2 class="subtitle">
-        Nuxt.js course for Udemy
-      </h2>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
         <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
