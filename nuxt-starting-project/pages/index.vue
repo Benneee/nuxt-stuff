c8b7c2f1e07955884391c40bd86c5d09278829df
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2022-07-02 10:48:48 +0100

    Feat: navigation and routing setup

--- nuxt-starting-project/pages/index.vue
+++ nuxt-starting-project/pages/index.vue
@@ -1,15 +1,6 @@
 <template>
   <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        Playing around with Nuxt.js... again
-      </h1>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
-    </div>
+    <nuxt-link to="/users">Users</nuxt-link>
   </section>
 </template>
 
--- nuxt-starting-project/pages/users/_id/index.vue
+++ nuxt-starting-project/pages/users/_id/index.vue
@@ -1,5 +1,5 @@
-.<template>
-  <h1>The users page!</h1>
+<template>
+  <h1> A single user with ID {{ $route.params.id }} </h1>
 </template>
 
 <script>
