3f953d5c86d1a8863632c1a02bc7645cb00b9714
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2022-07-02 11:37:45 +0100

    Feat: more on routing

---
+++ nuxt-starting-project/pages/products/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <nuxt-link to="/products/1">First product</nuxt-link> <br>
+    <nuxt-link to="/products/2">Second product</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
