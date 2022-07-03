5604da485e5036cf551ed65cd2803890e2b794df
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2022-07-03 13:47:46 +0100

    Feat: bloggr's architecture

---
+++ bloggr/pages/about/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="about-page">
+    <h1>An Absolutely Fantastic "About Me" Page!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, cupiditate? Consequuntur, necessitatibus!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, cupiditate? Consequuntur, necessitatibus!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, cupiditate? Consequuntur, necessitatibus!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, cupiditate? Consequuntur, necessitatibus!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, cupiditate? Consequuntur, necessitatibus!</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  .about-page {
+    text-align: center;
+    padding: 30px;
+  }
+</style>
