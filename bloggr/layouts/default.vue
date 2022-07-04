b322c1f015e3e3da78c407307a22c1db6df50d02
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2022-07-04 22:08:20 +0100

    Feat: added the header component

--- bloggr/layouts/default.vue
+++ bloggr/layouts/default.vue
@@ -1,11 +1,30 @@
 <template>
   <section>
+    <TheHeader @sidenavToggle="displaySideNav = !displaySideNav"/>
+    <TheSidenav
+      :show="displaySideNav"
+      @close="displaySideNav = false"
+    />
     <nuxt />
   </section>
 </template>
 
 <script>
+import TheHeader from '../components/Navigation/TheHeader.vue'
+import TheSidenav from '../components/Navigation/TheSidenav.vue'
 export default {
+  components: {
+    TheHeader,
+    TheSidenav
+  },
+
+  data() {
+    return {
+      displaySideNav: false,
+    }
+  },
+
+  methods: {}
 
 }
 </script>
