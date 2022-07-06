b2e8cc132f0ae60ab3e04e2f1352c1ec8066969f
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2022-07-06 23:55:26 +0100

    Feat: added an updated feature in the admin section

---
+++ bloggr/pages/admin/_postId/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="admin-post-page">
+    <section class="update-form">
+      <AdminPostForm :post="loadedPost" />
+    </section>
+  </div>
+</template>
+
+<script>
+import AdminPostForm from "@/components/Admin/AdminPostForm.vue";
+
+export default {
+  components: {
+    AdminPostForm
+  },
+
+  data() {
+    return {
+      loadedPost: {
+        author: "Ben Nk",
+        title: "My awesome post",
+        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est culpa distinctio voluptatem  quas aperiam vitae soluta possimus ipsum provident commodi tenetur eaque repellat iusto, reiciendis rem excepturi suscipit. Itaque, praesentium facere dicta eligendi atque voluptatum quo. Asperiores error, exercitationem dolores animi veniam nam minus et dicta repellendus aut labore quidem?",
+        thumbnailLink: 'https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .update-form {
+    width: 90%;
+    margin: 20px auto;
+  }
+  @media (min-width: 768px) {
+    .update-form {
+      width: 500px;
+    }
+  }
+</style>
