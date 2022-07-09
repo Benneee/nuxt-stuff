7dd1c4d3d6671a8e3325f991a852909e922c733b
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2022-07-09 16:17:51 +0100

    Feat: using asyncData in the posts page

--- bloggr/pages/posts/index.vue
+++ bloggr/pages/posts/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="posts-page">
-    <PostList />
+    <PostList :posts="loadedPosts"/>
   </div>
 </template>
 
@@ -10,6 +10,33 @@ import PostList from "../../components/Posts/PostList.vue";
 export default {
   components: {
     PostList
+  },
+
+  asyncData(context, callback) {
+    setTimeout(() => {
+      callback(null, {
+        loadedPosts: [
+            {
+            id: 1,
+            thumbnail:'https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg',
+            title:"Hello there!",
+            previewText:"This is my first post!",
+            },
+            {
+              id: 2,
+              thumbnail:'https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg',
+              title:"Hello again now!",
+              previewText:"This is my second post!",
+            },
+            {
+              id: 3,
+              thumbnail:'https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg',
+              title:"Hello for the third time!",
+              previewText:"This is my third post!"
+            }
+        ]
+      })
+    })
   }
 }
 </script>
