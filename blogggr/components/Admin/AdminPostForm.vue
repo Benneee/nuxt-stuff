e3c34e3713a37b5480c9216fc054ac48a17017c1
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2022-07-11 18:17:02 +0100

    Feat: fetch all posts from our backend

--- blogggr/components/Admin/AdminPostForm.vue
+++ blogggr/components/Admin/AdminPostForm.vue
@@ -2,13 +2,19 @@
     <form @submit.prevent="onSave">
       <AppControlInput v-model="editedPost.author">Author Name</AppControlInput>
       <AppControlInput v-model="editedPost.title">Title</AppControlInput>
-      <AppControlInput v-model="editedPost.thumbnailLink">Thumbnail Link</AppControlInput>
+      <AppControlInput v-model="editedPost.thumbnail">Thumbnail Link</AppControlInput>
 
       <AppControlInput
         control-type="textarea"
         v-model="editedPost.content"
       >
-      Content
+        Content
+      </AppControlInput>
+      <AppControlInput
+        control-type="textarea"
+        v-model="editedPost.previewText"
+      >
+        Preview Text
       </AppControlInput>
 
       <AppButton type="submit">Save</AppButton>
@@ -45,8 +51,9 @@ export default {
       editedPost: this.post ? { ...this.post } : {
         author: '',
         title: '',
-        thumbnailLink: '',
-        content: ''
+        thumbnail: '',
+        content: '',
+        previewText: ''
       }
     }
   },
