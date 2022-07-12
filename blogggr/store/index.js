475a3b918b4f936517caf245ea7a86bc2e71cf2f
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2022-07-12 18:25:25 +0100

    Feat: moving creation and update of code to vuex

--- blogggr/store/index.js
+++ blogggr/store/index.js
@@ -13,12 +13,21 @@ export const getters = {
 export const mutations = {
   setPosts(state, posts) {
     state.loadedPosts = posts
+  },
+
+  addPost(state, post) {
+    state.loadedPosts.push(post)
+  },
+
+  editPost(state, editedPost) {
+    const postIndex = state.loadedPosts.findIndex(post => post.id === editedPost.id)
+    state.loadedPosts[postIndex] = editedPost
   }
 }
 
 export const actions = {
   // This fetches the data once at the start of the application and can be used app-wide
-  nuxtServerInit(vuexContext, context) {
+  async nuxtServerInit(vuexContext, context) {
     return axios.get("https://blogggr-1ddbc-default-rtdb.firebaseio.com/posts.json")
       .then(resp => {
         const postsArray = [];
@@ -33,5 +42,22 @@ export const actions = {
 
   setPosts({ commit }, posts) {
     commit('setPosts', posts)
+  },
+
+  async addPost(context, post) {
+    const createdPost = { ...post, updatedDate: new Date() }
+    return axios.post("https://blogggr-1ddbc-default-rtdb.firebaseio.com/posts.json", createdPost)
+    .then(res => {
+        context.commit('addPost', { ...createdPost, id: res.data.name })
+    })
+    .catch(error => console.error("error: ", error))
+  },
+
+  async editPost(context, editedPost) {
+    return axios.put(`https://blogggr-1ddbc-default-rtdb.firebaseio.com/posts/${editedPost.id}.json`, editedPost)
+    .then(() => {
+      context.commit('editPost', editedPost)
+    })
+    .catch(error => context.error(error))
   }
 }
